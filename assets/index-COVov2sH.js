210da15241cefabab2e7ffec0a8d78b871610e99
unknown <bonyklade@gmail.com>
2024-12-25 00:21:05 +0200

    test

--- assets/index-COVov2sH.js
+++ assets/index-COVov2sH.js
@@ -42527,7 +42527,7 @@ const Dn = {
     // BLOCKCHAIN_API_RPC_URL: "https://rpc.walletconnect.org",
     // PULSE_API_URL: "https://pulse.walletconnect.org",
     // W3M_API_URL: "https://api.web3modal.org",
-    CHAIN: { EVM: "evm", SOLANA: "solana" },
+    // CHAIN: { EVM: "evm", SOLANA: "solana" },
     // CHAIN_NAME: { EVM: "Ethereum", SOLANA: "Solana" },
   };
 function bl(t, e) {
