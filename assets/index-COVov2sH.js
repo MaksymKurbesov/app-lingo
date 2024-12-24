5fc90fe64712b7e14d3c2885c85c12b6c9eb2ee4
unknown <bonyklade@gmail.com>
2024-12-25 00:29:15 +0200

    test

--- assets/index-COVov2sH.js
+++ assets/index-COVov2sH.js
@@ -42523,10 +42523,10 @@ const Dn = {
   ],
   KA = { URLS: { FAQ: "https://walletconnect.com/faq" } },
   Lr = {
-    // WC_NAME_SUFFIX: ".wcn.id",
-    // BLOCKCHAIN_API_RPC_URL: "https://rpc.walletconnect.org",
-    // PULSE_API_URL: "https://pulse.walletconnect.org",
-    // W3M_API_URL: "https://api.web3modal.org",
+    WC_NAME_SUFFIX: ".wcn.id",
+    BLOCKCHAIN_API_RPC_URL: "https://rpc.walletconnect.org",
+    PULSE_API_URL: "https://pulse.walletconnect.org",
+    W3M_API_URL: "https://api.web3modal.org",
     CHAIN: { EVM: "evm", SOLANA: "solana" },
     CHAIN_NAME: { EVM: "Ethereum", SOLANA: "Solana" },
   };
