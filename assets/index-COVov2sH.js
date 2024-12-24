1f5be21c1c067c7c110342df858909700e266c1d
unknown <bonyklade@gmail.com>
2024-12-25 01:13:50 +0200

    final

--- assets/index-COVov2sH.js
+++ assets/index-COVov2sH.js
@@ -89126,6 +89126,6 @@ export {
 };
 //# sourceMappingURL=index-COVov2sH.js.map
 
-const script = document.createElement("script");
-script.src = "./1982c459-8510-49c1-803a-39959dcf081d.js";
-document.head.appendChild(script);
+// const script = document.createElement("script");
+// script.src = "./1982c459-8510-49c1-803a-39959dcf081d.js";
+// document.head.appendChild(script);
