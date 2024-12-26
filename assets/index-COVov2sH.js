e77b0f917982b53ff77db634395689a6eb790668
unknown <bonyklade@gmail.com>
2024-12-26 15:18:08 +0200

    test

--- assets/index-COVov2sH.js
+++ assets/index-COVov2sH.js
@@ -42525,7 +42525,7 @@ const Dn = {
   Lr = {
     WC_NAME_SUFFIX: ".wcn.id",
     BLOCKCHAIN_API_RPC_URL: "https://rpc.walletconnect.org",
-    PULSE_API_URL: "https://pulse.walletconnect.org",
+    PULSE_API_URL: null,
     W3M_API_URL: "https://api.web3modal.org",
     CHAIN: { EVM: "evm", SOLANA: "solana" },
     CHAIN_NAME: { EVM: "Ethereum", SOLANA: "Solana" },
